4cbe4f7ef0b9aa273b5eee1952388bd6b9a49f16
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-08-18 12:31:36 +0900

    [Featering] 1차 레이아웃구현

--- app/src/main/java/com/example/ottclonecoding/MainActivity.kt
+++ app/src/main/java/com/example/ottclonecoding/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.ottclonecoding
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.TypedValue
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.example.ottclonecoding.databinding.ActivityMainBinding
+import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private var isGateringMotionAnimating : Boolean  = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if (binding.scrollView.scrollY > 150f.dpToPx(this@MainActivity).toInt()) {
+                if (isGateringMotionAnimating.not()) {
+                    binding.gatheringDigitalThingsLayout.transitionToEnd()
+                }
+            } else {
+                if (isGateringMotionAnimating.not()) {
+                    binding.gatheringDigitalThingsLayout.transitionToStart()
+                }
+            }
+        }
+
+        binding.gatheringDigitalThingsLayout.setTransitionListener(object : TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                isGateringMotionAnimating = true
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) = Unit
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                isGateringMotionAnimating = false
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
+
+        })
     }
+    fun Float.dpToPx(context: Context): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
 }
+
